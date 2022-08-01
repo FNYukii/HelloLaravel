5154751f123a9c505ae050241f4be171c599c8d1
Yu357 <forestgreen14689@gmail.com>
2022-08-01 21:37:15 +0900

    feat: Controller使ってみた

--- routes/web.php
+++ routes/web.php
@@ -13,14 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('top');
-});
+// Route::get('/', function () {
+//     return view('top');
+// });
 
-Route::get('/about', function () {
-    return view('about');
-});
+// Route::get('/about', function () {
+//     return view('about');
+// });
 
-Route::get('/contact', function () {
-    return view('contact');
-});
+// Route::get('/contact', function () {
+//     return view('contact');
+// });
+
+Route::get('/', [\App\Http\Controllers\IndexController::class, 'show']);
