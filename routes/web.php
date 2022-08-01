24a41e7d903051d22f4009286443c511a8c73a05
Yu357 <forestgreen14689@gmail.com>
2022-08-01 21:46:31 +0900

    clean: 元に戻した

--- routes/web.php
+++ routes/web.php
@@ -13,18 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('top');
-// });
+Route::get('/', function () {
+    return view('top');
+});
 
-// Route::get('/about', function () {
-//     return view('about');
-// });
+Route::get('/about', function () {
+    return view('about');
+});
 
-// Route::get('/contact', function () {
-//     return view('contact');
-// });
-
-Route::get('/sample', [\App\Http\Controllers\IndexController::class, 'show']);
-
-Route::get('/sample/{id}', [\App\Http\Controllers\IndexController::class, 'showId']);
+Route::get('/contact', function () {
+    return view('contact');
+});
