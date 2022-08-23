8804b29b8d9d421132122bf181df4cc73332ed08
Yu357 <forestgreen14689@gmail.com>
2022-08-24 02:20:56 +0900

    clean: コメント追加

--- app/Http/Controllers/CreateCommentController.php
+++ app/Http/Controllers/CreateCommentController.php
@@ -8,6 +8,10 @@ class CreateCommentController extends Controller
 {
     public function __invoke(Request $request)
     {
-        
+        // TODO: HTTPリクエストのパラメータからCommentの内容を取得
+
+        // TODO: 新規Commentをデータベースに保存
+
+        // TODO: CreatePageにリダイレクト
     }
 }
