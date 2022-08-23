b97813189d80aa306255a4b84bfd38c7983f128c
Yu357 <forestgreen14689@gmail.com>
2022-08-23 23:44:18 +0900

    feat: UpdateページとDeleteページを作成

--- resources/views/Pages/delete.blade.php
+++ resources/views/Pages/delete.blade.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Create - Hello Laravel</title>
+  <title>Delete - Hello Laravel</title>
   <link rel="stylesheet" href="styles/style.css">
 </head>
 <body>
@@ -13,9 +13,9 @@
 
   <main>
     <div class="large-container">
-      <h2>Create</h2>
+      <h2>Delete</h2>
     </div>
   </main>
-  
+
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,11 @@ Route::get('/', function () {
 Route::get('/create', function () {
     return view('Pages.create');
 });
+
+Route::get('/update/{commentId}', function () {
+    return view('Pages.update');
+});
+
+Route::get('/delete/{commentId}', function () {
+    return view('Pages.delete');
+});
