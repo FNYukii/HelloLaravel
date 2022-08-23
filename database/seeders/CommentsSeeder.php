99cad414a44451634a5834776a55000c76b74c6d
Yu357 <forestgreen14689@gmail.com>
2022-08-23 23:58:17 +0900

    feat: CommentsSeederを作成

---
+++ database/seeders/CommentsSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class CommentsSeeder extends Seeder
+{
+    
+    public function run()
+    {
+        DB::table('tweets')->insert([
+            'content' => Str::random(20),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+    }
+}
