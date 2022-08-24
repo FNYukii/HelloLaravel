c6f42cbe30beab8bb48ddfd3148b91a08d179e88
Yu357 <forestgreen14689@gmail.com>
2022-08-25 04:15:29 +0900

    2 Controllers

---
+++ app/Http/Controllers/UpdateCommentController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UpdateCommentController extends Controller
+{
+    // Todo: 指定されたCommentを編集する
+}
---
+++ app/Http/Controllers/UpdatePageController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UpdatePageController extends Controller
+{
+    // 指定されたCommentを取得
+
+    // Updateページを返す
+}
