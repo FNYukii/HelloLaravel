24c834ab3cb46f649d90f71db3ff7998a75e4be5
Yu357 <forestgreen14689@gmail.com>
2022-08-25 15:54:55 +0900

    feat: UpdateCommentRequestを作成

---
+++ app/Http/Requests/UpdateCommentRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateCommentRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true; // 認証を常に許可
+    }
+
+    public function rules()
+    {
+        return [
+            'content' => 'required|max:140' // バリデーションを設定
+        ];
+    }
+
+    // Comment column values
+    public function id(): int
+    {
+        return (int) $this->route('tweetId');
+    }
+
+    public function content(): string
+    {
+        return $this->input('content'); // HTTPリクエストパラメータを返す
+    }
+}
