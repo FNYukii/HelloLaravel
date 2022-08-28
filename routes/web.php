0a0fb07290297b1d97a0322993cac1d8594eb258
Yu357 <forestgreen14689@gmail.com>
2022-08-28 16:59:34 +0900

    chore: Laravel Breezeをインストール

--- routes/web.php
+++ routes/web.php
@@ -2,31 +2,23 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Top Page
-Route::get('/', \App\Http\Controllers\TopPageController::class)
-->name('topPage');
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
-// Create Page
-Route::get('/create', function () {
-    return view('Pages.CreatePage');
-})
-->name('createPage');
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::post('/createComment', \App\Http\Controllers\CreateCommentController::class)
-->name('createComment');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-// Update Page
-Route::get('/update/{commentId}', \App\Http\Controllers\UpdatePageController::class)
-->name('updatePage')
-->where('commentId', '[0-9]+');
-
-Route::post('/updateComment', \App\Http\Controllers\UpdateCommentController::class)
-->name('updateComment');
-
-// Delete Page
-Route::get('/delete/{commentId}', \App\Http\Controllers\DeletePageController::class)
-->name('deletePage')
-->where('commentId', '[0-9]+');
-
-Route::post('/deleteComment', \App\Http\Controllers\DeleteCommentController::class)
-->name('deleteComment');
+require __DIR__.'/auth.php';
