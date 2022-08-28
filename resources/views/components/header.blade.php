bc4449f08ffe539ecc460b7313759309a73eb963
Yu357 <forestgreen14689@gmail.com>
2022-08-28 18:11:15 +0900

    clean: 不要なコードを削除

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -12,11 +12,9 @@
                 <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
             </li>
 
-            @if (Route::has('register'))
             <li>
                 <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
             </li>
-            @endif
             @endauth
 
             @auth
